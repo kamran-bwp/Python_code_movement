de1976feb58780aee6b61902660f2a07f4e9329c
kamran-bwp <kamran.dcet@gmail.com>
2022-12-06 19:47:20 +0100

    hhhj

--- first_lecture.py
+++ first_lecture.py
@@ -1,5 +1,9 @@
-a = 2
-if a % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+#2.7 Write a program that reads in two integers and determines and prints whether the
+#first is a multiple of the second. (Hint: Use the modulus operator.)
+
+#a = int(input("Enter first integer: "))
+#b = int(input("Enter 2nd integer: "))
+
+#print(a, b)
+
+print("Hello world")
